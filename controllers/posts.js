4d50ca53c0a004e4bbff8db3f246e0fe9d61ab63
sergey <kirnozss@gmail.com>
2021-01-12 20:59:05 +0200

    all crud

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,10 +4,75 @@ exports.createPost = async (req, res, next) => {
     try {
         const data = req.body
         const date = Date.now()
-        const post = await new Post({...data, date})
-        
+        const post = await new Post({ ...data, date })
+
         await post.save()
         res.json(post)
+
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+}
+
+exports.getPosts = async (req, res, next) => {
+    try {
+        const posts = await Post.find()
+
+        res.json(posts)
+
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+}
+
+exports.getPostById = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        if (!post) {
+            throw new Error('Post not found')
+        }
+
+        res.json(post)
+
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+}
+
+exports.updatePost = async (req, res, next) => {
+    try {
+        const data = req.body
+
+        const post = await Post.findByIdAndUpdate(req.params.id, {
+            ...data
+        })
+
+        if (!post) {
+            throw new Error('Post not found')
+        }
+
+        res.json('post updated')
+
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+}
+
+exports.deletePost = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.id)
+
+        if (!post) {
+            throw new Error('Post not found')
+        }
+
+        await post.remove()
+        res.json("post deleted")
 
     } catch (e) {
         console.log(e)
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-const {createPost} = require('../controllers/posts')
+const {
+    getPosts,
+    createPost,
+    updatePost,
+    deletePost,
+    getPostById
+} = require('../controllers/posts')
+
 
 router.post('/', createPost)
+router.put('/:id', updatePost)
+router.get('/', getPosts)
+router.get('/:id', getPostById)
+router.delete('/:id', deletePost)
+
 
 module.exports = router
