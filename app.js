bfbbfc9142e04211ca5a9a94a3dc193860ec3e08
sergey <kirnozss@gmail.com>
2021-01-12 19:58:06 +0200

    connected database

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const config = require('./config/keys')
 
 const app = express()
+
+// database
+mongoose.connect(config.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+}).then(() => console.log('Database connected'))
+
+// middleware
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.json('Hello')
 })
 
 
-const port = 4000
+const port = config.PORT || 4000
 app.listen(port, () => {
     console.log(`Server has been started on port: ${port}`)
 })
